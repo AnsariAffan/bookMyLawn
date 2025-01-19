a1e651962bc0511ec5404dba8590bca5dd2177c7
mohammadaffan <mohammadaffan777@gmail.com>
2025-01-19 16:16:38 +0530

    added a stander settin optios icon

--- components/Settings.js
+++ components/Settings.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView, View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Avatar, List, TextInput, Dialog, Portal, Button, Provider } from 'react-native-paper';
+import { Text, Avatar, TextInput, Dialog, Portal, Button, Provider } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons'; // Importing Material Icons
 import * as ImagePicker from 'expo-image-picker';
 import { updateProfile, updatePassword } from 'firebase/auth'; // Import Firebase methods
 import { auth } from '../firebaseConfiguration/firebaseConfig';
@@ -127,91 +128,88 @@ const Settings = ({ navigation }) => {
   };
 
   return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.profileContainer}>
+        <TouchableOpacity onPress={pickImage}>
+          {image ? (
+            <Avatar.Image size={100} source={{ uri: image }} />
+          ) : (
+            <Avatar.Icon size={100} icon="account" />
+          )}
+        </TouchableOpacity>
+        <Text style={styles.username}>{username}</Text>
+        <Text style={styles.email}>{email}</Text>
+      </View>
 
-      <SafeAreaView style={styles.container}>
-        <View style={styles.profileContainer}>
-          <TouchableOpacity onPress={pickImage}>
-            {image ? (
-              <Avatar.Image size={100} source={{ uri: image }} />
-            ) : (
-              <Avatar.Icon size={100} icon="account" />
-            )}
-          </TouchableOpacity>
-          <Text style={styles.username}>{username}</Text>
-          <Text style={styles.email}>{email}</Text>
-        </View>
-        <View style={styles.section}>
+      <View style={styles.section}>
         <TouchableOpacity style={styles.item} onPress={() => showDialog('username')}>
           <View style={styles.iconTextContainer}>
-            <Text style={styles.icon}>✏️</Text>
+            <Icon name="edit" size={24} color="#333" style={styles.icon} />
             <Text style={styles.title}>Change Username</Text>
           </View>
         </TouchableOpacity>
-      
+
         <TouchableOpacity style={styles.item} onPress={showPasswordDialog}>
           <View style={styles.iconTextContainer}>
-            <Text style={styles.icon}>🔒</Text>
+            <Icon name="lock" size={24} color="#333" style={styles.icon} />
             <Text style={styles.title}>Change Password</Text>
           </View>
         </TouchableOpacity>
-      
+
         <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('AboutContactUs')}>
           <View style={styles.iconTextContainer}>
-            <Text style={styles.icon}>📄</Text>
+            <Icon name="info" size={24} color="#333" style={styles.icon} />
             <Text style={styles.title}>Contact Us & About Us</Text>
           </View>
         </TouchableOpacity>
-      
+
         <TouchableOpacity style={styles.item} onPress={handleLogout}>
           <View style={styles.iconTextContainer}>
-            <Text style={styles.icon}>🚪</Text>
+            <Icon name="exit-to-app" size={24} color="#333" style={styles.icon} />
             <Text style={styles.title}>Logout</Text>
           </View>
         </TouchableOpacity>
       </View>
-      
 
-        <Portal>
-          <Dialog visible={visible} onDismiss={hideDialog}>
-            <Dialog.Title>Edit {editType.charAt(0).toUpperCase() + editType.slice(1)}</Dialog.Title>
-            <Dialog.Content>
-              <TextInput
-                label={`New ${editType.charAt(0).toUpperCase() + editType.slice(1)}`}
-                value={tempValue}
-                onChangeText={(text) => setTempValue(text)}
-              />
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button onPress={hideDialog}>Cancel</Button>
-              <Button onPress={handleSave}>Save</Button>
-            </Dialog.Actions>
-          </Dialog>
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog}>
+          <Dialog.Title>Edit {editType.charAt(0).toUpperCase() + editType.slice(1)}</Dialog.Title>
+          <Dialog.Content>
+            <TextInput
+              label={`New ${editType.charAt(0).toUpperCase() + editType.slice(1)}`}
+              value={tempValue}
+              onChangeText={(text) => setTempValue(text)}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Cancel</Button>
+            <Button onPress={handleSave}>Save</Button>
+          </Dialog.Actions>
+        </Dialog>
 
-          {/* Password change dialog */}
-          <Dialog visible={passwordDialogVisible} onDismiss={hidePasswordDialog}>
-        
-            <Dialog.Content>
-              <TextInput
-                label="Current Password"
-                secureTextEntry
-                value={currentPassword}
-                onChangeText={(text) => setCurrentPassword(text)}
-              />
-              <TextInput
-                label="New Password"
-                secureTextEntry
-                value={newPassword}
-                onChangeText={(text) => setNewPassword(text)}
-              />
-            </Dialog.Content>
-            <Dialog.Actions>
-              <Button onPress={hidePasswordDialog}>Cancel</Button>
-              <Button onPress={handleChangePassword}>Save</Button>
-            </Dialog.Actions>
-          </Dialog>
-        </Portal>
-      </SafeAreaView>
-   
+        {/* Password change dialog */}
+        <Dialog visible={passwordDialogVisible} onDismiss={hidePasswordDialog}>
+          <Dialog.Content>
+            <TextInput
+              label="Current Password"
+              secureTextEntry
+              value={currentPassword}
+              onChangeText={(text) => setCurrentPassword(text)}
+            />
+            <TextInput
+              label="New Password"
+              secureTextEntry
+              value={newPassword}
+              onChangeText={(text) => setNewPassword(text)}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hidePasswordDialog}>Cancel</Button>
+            <Button onPress={handleChangePassword}>Save</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </SafeAreaView>
   );
 };
 
@@ -219,7 +217,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 15,
-    backgroundColor:"white"
+    backgroundColor: 'white',
   },
   profileContainer: {
     alignItems: 'center',
@@ -230,28 +228,25 @@ const styles = StyleSheet.create({
     marginTop: 10,
     fontSize: 18,
     fontWeight: 'bold',
-  color:"black"
+    color: 'black',
   },
   email: {
     fontSize: 16,
-     color:"black"
+    color: 'black',
   },
   section: {
     marginTop: 20,
-
-  
     paddingTop: 10,
   },
   item: {
     paddingVertical: 10,
-  
   },
   iconTextContainer: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   icon: {
-    fontSize: 20, // Adjust the size of the icon
+    fontSize: 24, // Adjust the size of the icon
     marginRight: 10,
   },
   title: {
